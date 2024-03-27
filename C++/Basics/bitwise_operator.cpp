033de50454e31cfc40a659b9a26e46a88033f6d4
JATINGARG25 <gargjatin519@gmail.com>
2024-03-27 14:50:46 +0530

    .

---
+++ C++/Basics/bitwise_operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 4;
+    int b = 6;
+
+    cout<< "a & b = " << (a&b) << endl;
+    cout<< "a | b = " << (a|b) << endl;
+    cout<< "a ^ b = " << (a^b) << endl;
+    cout<< "~a = " << (~a) << endl;
+
+    cout<< (17<<1) << endl;         // left shift -- basically multiply by 2
+    cout<< (17<<2) << endl;         // left shift
+    cout<< (19>>1) << endl;         // right shift -- basically divide by 2
+    cout<< (19>>2) << endl;         // right shift
+
+}
